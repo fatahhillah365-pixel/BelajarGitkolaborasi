a693efadcf3199e6299b6410eebd1425f7e0db54
fatahhillah365-pixel <fatahhillah365@gmail.com>
2026-05-11 14:54:47 +0700

    fix : playerdata

--- Assets/Scripts/ScriptableObject/PlayerData.cs
+++ Assets/Scripts/ScriptableObject/PlayerData.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PlayerData", menuName = "Game/Player Data")]
 public class PlayerData : ScriptableObject
 {
+    // Jumlah HP maksimum pemain. Digunakan untuk menyetel HP awal dan batas atas bar HP.
     public float maxHP;
+
+    // Kecepatan gerak pemain. Digunakan oleh PlayerController untuk mengatur seberapa cepat pemain bergerak.
     public float moveSpeed;
 }
